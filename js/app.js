23ec7af2ac0e0288b8318215b04353612b8e5c7d
Ruslan Zherebytskyi <rusikmvp@gmail.com>
2020-03-07 21:26:44 +0100

    add city searcher

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
-console.log('hello world!')
+const APP_KEY = 'fd70466ba1e5df5c105fdb9879e197c7';
+const searchBtn = document.querySelector('.search-button')
+const searchItem = document.querySelector('.search-subitem')
+
+searchBtn.addEventListener('click', async (e)=>{
+    const requestUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchItem.value}&appid=${APP_KEY}`
+    const response = await fetch(requestUrl)
+    const data = await response.json();
+    const 
+    for(city of data){
+      console.log(city.name)
+    }
+    
+})
+
+
+
